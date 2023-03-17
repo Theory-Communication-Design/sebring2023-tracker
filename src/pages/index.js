e5e9da292d9681720b06eeaf2ccc2ad817f4ef97
Josh Suson <josh.suson@gmail.com>
2023-03-16 21:46:04 -0400

    working on firebase errors

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,6 +30,7 @@ export default function Home({ initialAppData, initialSiteData }) {
   const [siteData, setSiteData] = useState(initialSiteData);
 
   useEffect(() => {
+    console.log(process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
     const siteUnsub = onSnapshot(sebringSite, (querySnapshot) => {
       setSiteData(querySnapshot.docs.map((doc) => doc.data()));
     });
